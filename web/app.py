aa58b04ad3863b5a0592b5880c3943127793a070
Anandhu Biju <anandhubijuabk7598@gmail.com>
2022-06-23 13:39:46 +0530

    bootstrap style added successfully

--- web/app.py
+++ web/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MYSQL
+import yaml
 
-app= Flask(__name__)
+app= Flask(__name__,template_folder='template')
 
-@app.route('/')
-def index():
-    return 'Flask'
+db=yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+
+mysql=MYSQL(app)
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('recipe.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
