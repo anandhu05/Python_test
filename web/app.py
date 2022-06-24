ca99da4423c6e46d7c3940bbc2e27922de690c6d
Anandhu Biju <anandhubijuabk7598@gmail.com>
2022-06-24 12:34:12 +0530

    database connection established

--- web/app.py
+++ web/app.py
@@ -1,17 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
-import getdata
-import mysql.connector
+
+
 app= Flask(__name__,template_folder='template')
-connection=mysql.connector.connect(host='localhost',database='recipeapp',user="root",password="root@123")
-cursor1=connection.cursor()
-#mysql=MySQL(app)
-@app.route('/',methods=['GET','POST'])
-def home():
+
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root@12'
+app.config['MYSQL_DB'] = 'recipeapp'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM food_items")
+    data = cur.fetchall()
+    cur.close()
+
     return render_template('recipe.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == "POST":
+        flash("Data Inserted Successfully")
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
 
 
 def getData():
